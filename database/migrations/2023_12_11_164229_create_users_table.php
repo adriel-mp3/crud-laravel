559e38a8652c0b8743f630eb3e2997dd0d9e21da
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-12-12 13:42:01 -0300

    feat: create users table migration

---
+++ database/migrations/2023_12_11_164229_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 50);
+            $table->date('data_nascimento');
+            $table->string('cpf_cnpj')->unique()->regex('/^\d{11,14}$/');
+            $table->string('foto')->nullable();
+            $table->string('nome_social', 50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
